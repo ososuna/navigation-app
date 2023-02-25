0566a7f01caeca759e06c43777c8d03c8715a68b
ososuna <oswaldo.adrian@live.com.mx>
2023-02-25 14:26:28 -0600

    stack navigator installation

---
+++ src/screens/Page1Screen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Page1Screen = () => {
+  return (
+    <View><Text>Page1Screen</Text></View>
+  );
+};
---
+++ src/screens/Page2Screen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Page2Screen = () => {
+  return (
+    <View><Text>Page2Screen</Text></View>
+  );
+};
