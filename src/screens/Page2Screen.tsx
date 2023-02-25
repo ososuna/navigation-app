8a71049affa26ae7d69bfcb9c851372557bab530
ososuna <oswaldo.adrian@live.com.mx>
2023-02-25 15:07:58 -0600

    stack navigator styles

--- src/screens/Page2Screen.tsx
+++ src/screens/Page2Screen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import { styles } from '../theme/appTheme';
@@ -7,9 +7,16 @@ export const Page2Screen = () => {
 
   const navigator = useNavigation();
 
+  useEffect(() => {
+    navigator.setOptions({
+      title: 'Hello world',
+      headerBackTitle: 'Back',
+    });
+  }, );
+
   return (
     <View style={ styles.globalMargin }>
-      <Text>Page2Screen</Text>
+      <Text style={ styles.title }>Page2Screen</Text>
       <Button
         title="Go to page 3"
         onPress={ () => navigator.navigate('Page3Screen') }
