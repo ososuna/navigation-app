d22e0eb225da7ad45a807fb93e333bbdc47074aa
oswi <oswaldo.adrian@live.com.mx>
2023-03-01 21:09:21 -0600

    bug fixes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/navigator/StackNavigator';
+import { BasicSideMenu } from './src/navigator/BasicSideMenu';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <BasicSideMenu />
     </NavigationContainer>
   );
 };
