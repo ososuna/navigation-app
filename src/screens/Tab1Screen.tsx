ceda45bb91cb435fc20dcc6214ae7efaf70d3fb7
oswi <oswaldo.adrian@live.com.mx>
2023-03-24 14:53:13 -0600

    added ionicons

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { colors, styles } from '../theme/appTheme';
 
 export const Tab1Screen = () => {
 
@@ -8,8 +10,11 @@ export const Tab1Screen = () => {
   }, []);
 
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={ styles.globalMargin }>
+      <Text style={ styles.title }>Icons</Text>
+        <Text>
+          <Icon name="airplane-outline" size={80} color={ colors.primary } />
+        </Text>
     </View>
   );
 };
