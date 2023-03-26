52c30c31c9c79efd4cef3585ce5326cfdaad7848
oswi <oswaldo.adrian@live.com.mx>
2023-03-26 13:00:06 -0600

    dispatching actions

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
+import { authReducer } from './authReducer';
 
 export interface AuthState {
   isLoggedIn: boolean;
@@ -19,10 +20,16 @@ export const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider = ({ children }: { children: JSX.Element }) => {
 
+  const [authState, dispatch] = useReducer(authReducer, authInitialState);
+
+  const signIn = () => {
+    dispatch({ type: 'signIn' });
+  };
+
   return (
     <AuthContext.Provider value={{
-      authState: authInitialState,
-      signIn: () => {},
+      authState,
+      signIn,
     }}>
       { children }
     </AuthContext.Provider>
